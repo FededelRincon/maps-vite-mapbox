e059f6f84c24fd8d309ee1efc62e20c6795b3e55
FDR <delrincon@gmail.com>
2022-06-23 20:01:03 -0300

    context set it up

---
+++ src/MapsApp.tsx
@@ -0,0 +1,12 @@
+import { PlacesProvider } from "./context";
+import './MapsApp.css';
+
+
+export const MapsApp = () => {
+
+    return (
+        <PlacesProvider>
+            <h1>Hola mundo de nuevo</h1>
+        </PlacesProvider>
+    )
+}
