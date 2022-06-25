06340f959eb5f105a7d584639e5d562d44eab262
FDR <delrincon@gmail.com>
2022-06-25 20:24:30 -0300

    showing mapbox map on screen with your coords

--- src/components/MapView.tsx
+++ src/components/MapView.tsx
@@ -1,11 +1,27 @@
-import { useContext } from "react"
+import { useContext, useEffect, useRef } from "react"
+import { Map } from "mapbox-gl";
+
 import { PlacesContext } from '../context/places/PlacesContext';
+
 import { Loading } from "./";
+
 
 
 export const MapView = () => {
 
     const { isLoading, userLocation } = useContext(PlacesContext);
+    const mapDiv = useRef<HTMLDivElement>(null);
+
+    useEffect(() => {
+        if( !isLoading ) {
+            const map = new Map({
+                container: mapDiv.current!,
+                style: 'mapbox://styles/mapbox/streets-v11', // style URL
+                center: userLocation, 
+                zoom: 15 // starting zoom
+            });
+        }
+    }, [isLoading])
 
     if( isLoading ) {
         return (
@@ -14,7 +30,16 @@ export const MapView = () => {
     }
 
     return (
-        <div>
+        <div 
+            ref={ mapDiv }
+            style={{
+                height: '100vh',
+                left: 0,
+                position: 'fixed',
+                top: 0,
+                width: '100vw',
+            }}
+        >
             { userLocation?.join(', ') }
         </div>
     )
--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,16 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+
+import mapboxgl from 'mapbox-gl'; 
+
 import { MapsApp } from './MapsApp'
+
 import './index.css'
+
+ 
+mapboxgl.accessToken = 'pk.eyJ1IjoicGFxdWV0aW5obyIsImEiOiJja2sycDc4YzAxM2kyMm9tZm8ybWg3emdrIn0.6XhGimBpY4H5p_zjExagpg';
+
+
 
 if( !navigator.geolocation ) {
   alert('tu navegador no tiene opcion de Geolocalizacion')
