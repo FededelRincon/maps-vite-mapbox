5934b1bfd47a0d448f3086f21330ebbbd9022afc
FDR <delrincon@gmail.com>
2022-06-29 20:23:04 -0300

    move marker center from results + update css to cyan

--- src/apis/searchApi.ts
+++ src/apis/searchApi.ts
@@ -4,7 +4,7 @@ import axios from "axios";
 const searchApi = axios.create({
     baseURL: 'https://api.mapbox.com/geocoding/v5/mapbox.places',
     params: {
-        limit: 5,
+        limit: 4,
         languaje: 'es', 
         access_token: 'pk.eyJ1IjoicGFxdWV0aW5obyIsImEiOiJja2sycDc4YzAxM2kyMm9tZm8ybWg3emdrIn0.6XhGimBpY4H5p_zjExagpg',
     }
--- src/components/SearchResults.tsx
+++ src/components/SearchResults.tsx
@@ -1,12 +1,26 @@
-import { useContext } from 'react';
-import { PlacesContext } from '../context';
+import { useContext, useState } from 'react';
+import { MapContext, PlacesContext } from '../context';
 import { LoadingPlaces } from './';
+import { Feature } from '../interfaces/places';
 
 
 
 export const SearchResults = () => {
 
+    const [activeId, setActiveId] = useState('')
+
     const { isLoadingPlaces, places } = useContext(PlacesContext)
+
+    const { map } = useContext(MapContext)
+
+    const onPlaceClicked = ( place: Feature ) => {
+        setActiveId(place.id);
+        const [ lng, lat] = place.center;
+        map?.flyTo({
+            zoom: 14,
+            center: [lng, lat]
+        })
+    }
 
     if (isLoadingPlaces) {
         return <LoadingPlaces />
@@ -23,17 +37,17 @@ export const SearchResults = () => {
                 places.map( (place) => (
                     <li
                         key={ place.id }
-                        className="list-group-item list-group-item-action"
+                        className={`list-group-item list-group-item-action pointer ${ activeId === place.id ? 'active' : '' }`}
+                        onClick={ () => onPlaceClicked(place) }
                     >
                         <h6>{ place.text }</h6>
                         <p 
-                            className="text-muted"
                             style={{ fontSize: '12px'}}
                         >
                             { place.place_name }
                         </p>
 
-                        <button className="btn btn-outline-info btn-sm">
+                        <button className={`btn btn-sm ${ activeId === place.id ? 'btn-outline-light' : 'btn-outline-info'} `}>
                             Direcciones
                         </button>
                     </li>
