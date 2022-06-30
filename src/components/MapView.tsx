3db1056ad224fba89eaf8e87f5e3305238e1e32e
FDR <delrincon@gmail.com>
2022-06-29 21:02:05 -0300

    git warning from map + update css from searchField

--- src/components/MapView.tsx
+++ src/components/MapView.tsx
@@ -43,7 +43,7 @@ export const MapView = () => {
                 width: '100vw',
             }}
         >
-            { userLocation?.join(', ') }
+            {/* { userLocation?.join(', ') } */}
         </div>
     )
 }
